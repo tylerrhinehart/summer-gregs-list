e6fe165287d8e5cbfca5799af562cebbbf23f143
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2017-08-07 14:06:03 -0600

    adds local storage

--- app/components/home/home-service.js
+++ app/components/home/home-service.js
@@ -1,21 +1,10 @@
 function HomeService() {
 
-    var homes = [{
-        description: 'awesome',
-        price: 100000,
-        sqft: 1500,
-        img: '//placehold.it/150x150'
-    }, {
-        description: 'super awesome',
-        price: 1000000,
-        sqft: 15000,
-        img: '//placehold.it/300x250'
-    }, {
-        description: 'maximum awesome',
-        price: 10000000,
-        sqft: 150000,
-        img: '//placehold.it/200x201'
-    }]
+    var homes = JSON.parse(localStorage.getItem('homes')) || []
+
+    function saveHomes(){
+        localStorage.setItem('homes', JSON.stringify(homes))
+    }
 
     this.getHomes = function(cb) {
         cb(JSON.parse(JSON.stringify(homes)))
@@ -23,5 +12,8 @@ function HomeService() {
 
     this.addHome = function(home){
         homes.push(home)
+        saveHomes()
     }
+
+
 }
